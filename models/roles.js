361de74fd70ec43d7c1d1e630e803c67dc00c2a2
Osama-tec <hashmiosama555@gmail.com>
2023-10-26 02:23:55 +0500

    roles added missing also

--- models/roles.js
+++ models/roles.js
@@ -1,28 +1,22 @@
 const mongoose = require('mongoose');
-
 const rolesSchema = new mongoose.Schema({
-
   userId: { type: String },
   roleName: { type: String, required: true },
   serviceStaff: {type: Boolean},
   viewExport: {type: Boolean},
-
   viewUser: {type: Boolean},
   addUser: {type: Boolean},
   editUser: {type: Boolean},
   deleteUser: {type: Boolean},
-
   viewRole: {type: Boolean},
   addRole: {type: Boolean},
   editRole: {type: Boolean},
   deleteRole: {type: Boolean},
-
   viewAllSupplier: {type: Boolean},
   viewOwnSupplier: {type: Boolean},
   addSupplier: {type: Boolean},
   editSupplier: {type: Boolean},
   deleteSupplier: {type: Boolean},
-
   viewAllCustomer: {type: Boolean},
   viewOwnCutomer: {type: Boolean},
   viewCustomerswithnosellfromonemonthonly: {type: Boolean},
@@ -33,14 +27,12 @@ const rolesSchema = new mongoose.Schema({
   addCustomer: {type: Boolean},
   editCustomer: {type: Boolean},
   deleteCustomer: {type: Boolean},
-
   viewProduct: {type: Boolean},
   addProduct: {type: Boolean},
   editProduct: {type: Boolean},
   deleteProduct: {type: Boolean},
   addOpeningStock: {type: Boolean},
   viewPurchasePrice: {type: Boolean},
-
   viewAllPurchaseStockAdjustment: {type: Boolean},
   viewOwnPurchaseStockAdjustment: {type: Boolean},
   addPurchaseStockAdjustment: {type: Boolean},
@@ -50,13 +42,11 @@ const rolesSchema = new mongoose.Schema({
   editPurchasePayment: {type: Boolean},
   deletePurchasePayment: {type: Boolean},
   updateStatus: {type: Boolean},
-
   viewAllPurchaseOrder: {type: Boolean},
   viewOwnPurchaseOrder: {type: Boolean},
   createPurchaseOrder: {type: Boolean},
   editPurchaseOrder: {type: Boolean},
   deletePurchaseOrder: {type: Boolean},
-
   viewPossell: {type: Boolean},
   addPossell: {type: Boolean},
   editPossell: {type: Boolean},
@@ -65,7 +55,6 @@ const rolesSchema = new mongoose.Schema({
   editProductdiscountfromposscreen: {type: Boolean},
   addeditPayment: {type: Boolean},
   printinvoice: {type: Boolean},
-
   viewAllSell: {type: Boolean},
   viewOwnSellOnly: {type: Boolean},
   viewPaidSellOnly: {type: Boolean},
@@ -75,7 +64,6 @@ const rolesSchema = new mongoose.Schema({
   addSell: {type: Boolean},
   updateSell: {type: Boolean},
   deleteSell: {type: Boolean},
-  
   addSellPayment: {type: Boolean},
   editSellPayment: {type: Boolean},
   deleteSellPayment: {type: Boolean},
@@ -86,42 +74,34 @@ const rolesSchema = new mongoose.Schema({
   accessAllSellReturn: {type: Boolean},
   accessOwnSellReturn: {type: Boolean},
   addEditInvoiceNumber: {type: Boolean},
-
   viewAllDrafts: {type: Boolean},
   viewOwnDrafts: {type: Boolean},
   editDraft: {type: Boolean},
   deleteDraft: {type: Boolean},
-
   viewAllQuotations: {type: Boolean},
   viewOwnQuotations: {type: Boolean},
   editQuotation: {type: Boolean},
   deleteQuotation: {type: Boolean},
-
   accessAllShipments: {type: Boolean},
   accessOwnShipments: {type: Boolean},
   viewCashRegister: {type: Boolean},
   closeCashRegister: {type: Boolean},
-
   viewBrand: {type: Boolean},
   addBrand: {type: Boolean},
   editBrand: {type: Boolean},
   deleteBrand: {type: Boolean},
-
   viewTaxRate: {type: Boolean},
   addTaxRate: {type: Boolean},
   editTaxRate: {type: Boolean},
   deleteTaxRate: {type: Boolean},
-
   viewUnit: {type: Boolean},
   addUnit: {type: Boolean},
   editUnit: {type: Boolean},
   deleteUnit: {type: Boolean},
-
   viewCategory: {type: Boolean},
   addCategory: {type: Boolean},
   editCategory: {type: Boolean},
   deleteCategory: {type: Boolean},
-
   viewPurchasesellreport: {type: Boolean},
   viewTaxreport: {type: Boolean},
   viewSuppliercustomerreport: {type: Boolean},
@@ -132,36 +112,38 @@ const rolesSchema = new mongoose.Schema({
   viewRegisterreport: {type: Boolean},
   viewSalesrepresentativereport: {type: Boolean},
   viewProductstockvalue: {type: Boolean},
-
   accessBusinessSettings: {type: Boolean},
   accessInvoiceSettings: {type: Boolean},
-
   accessAlleEpenses: {type: Boolean},
   viewOwnExpenseOnly: {type: Boolean},
   addExpense: {type: Boolean},
   editExpense: {type: Boolean},
   deleteExpense: {type: Boolean},
-
   viewHomeData: {type: Boolean},
-
   accessAccounts: {type: Boolean},
   editAccountTransaction: {type: Boolean},
   deleteAccountTransaction: {type: Boolean},
-
   defaultSellingPrice: {type: Boolean},
   retail: {type: Boolean},
   saleMan: {type: Boolean},
   localSale: {type: Boolean},
   minimumPrice: {type: Boolean},
   salePoint: {type: Boolean},
-
   viewRecipe: {type: Boolean},
   addRecipe: {type: Boolean},
   editRecipe: {type: Boolean},
   accessProduction: {type: Boolean},
-
+  viewAllVariations: {type: Boolean},
+  createVariation: {type: Boolean},
+  updateVariation: {type: Boolean},
+  deleteVariation: {type: Boolean},
+  getVariationById: {type: Boolean},
+  getAllWarranties: {type: Boolean},
+  createWarranty: {type: Boolean},
+  updateWarranty: {type: Boolean},
+  deleteWarranty: {type: Boolean},
+  getWarrantyById: {type: Boolean},
 });
 
 const Role = mongoose.model('Role', rolesSchema);
-
 module.exports = Role;
